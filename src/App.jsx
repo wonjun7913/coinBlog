3b6c421b8529ce5877e09b3e9b1261e99065c1b2
최원준 <cvcv7913@gmail.com>
2025-09-05 17:33:01 +0900

    ver 2.2 margin 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import RainAnimation from './RainAnimation';
 import './App.css'
 
-// 데이터와 컴포넌트를 분리했다고 가정!
 import { historyData } from './data';
 import HistorySection from './HistorySection';
 
